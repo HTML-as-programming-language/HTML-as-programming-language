8ac978316cb8d961d84dfe0337a0d93c9d489070
ErrorCode51 <43345236+errorcode51@users.noreply.github.com>
2018-10-05 15:18:06 +0200

    Added class expression (not finished)

---
+++ HTML_to_C_compiler/elements/expression.py
@@ -0,0 +1,32 @@
+from elements.element import Element
+
+
+class Expression(Element):
+    """"
+    HTML: <expression x="a > 50">
+    <ya-really>
+        <!-- if-code here -->
+    </ya-really>
+
+    <maybe x="a < 10">
+        <!-- else-if code here -->
+    </maybe>
+
+    <no-wai>
+        <!-- else code here -->
+    </no-wai>
+    </expression>
+
+    C: if (a > 50){
+    //code goes here
+    }
+    else if (a < 10) {
+    //else-if code here
+    }
+    else {
+    //else code here
+    }
+    """
+
+    def to_c(self):
+        pass
