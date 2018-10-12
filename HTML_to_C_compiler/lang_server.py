c31a6cb875707684812e4b3edd9ca726712a7e23
hilkojj <hilkojj@outlook.com>
2018-10-12 20:48:50 +0200

    showMessage

--- HTML_to_C_compiler/lang_server.py
+++ HTML_to_C_compiler/lang_server.py
@@ -12,6 +12,13 @@ class LanguageServer(JsonRpcServer):
                 "textDocumentSync": 1  # 0 = None, 1 = full, 2 = incremental
             }
         })
+        self.send({
+            "method": "window/showMessage",
+            "params": {
+                "type": 3,
+                "message": "HTMLC Language Server started"
+            }
+        })
 
     def text_document__did_open(self, request):
         self.send({
