549c3fb17a211f69e9bce3dc024bc8acb389e05c
hilkojj <hilkojj@outlook.com>
2018-10-12 22:59:50 +0200

    use htmlclangsvr instead of python bla bla bla

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -12,26 +12,15 @@ let client: LanguageClient;
 // this method is called when the extension is activated
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('"html-as-programming-language" is now active!');
 
     var config = vscode.workspace.getConfiguration('htmlc');
-    var compilerDir: string = config.get("compilerDir") || "./";
-    compilerDir.replace("\\", "/");
-    console.log(`compilerDir: ${compilerDir}`);
-    var langServer = startLangServer(compilerDir);
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
+    
     let disposable = vscode.commands.registerCommand('extension.kikker', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
         vscode.window.showInformationMessage('Ik ben een kikker!');
     });
 
+    startLangServer();
     context.subscriptions.concat([
         client.start(),
         disposable
@@ -45,11 +34,10 @@ export function deactivate() {
         client.stop();
 }
 
-function startLangServer(dir: string) {
+function startLangServer() {
 
     const serverOptions: ServerOptions = {
-        command: "python",
-        args: [dir + (dir.endsWith("/") ? "" : "/") + "lang_server.py"]
+        command: "htmlclangsvr"
 	};
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: ["html", "plaintext"]
