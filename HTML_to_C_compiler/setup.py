b6c00daa50a30944ed4a95be6bfae94c195ac779
hilkojj <hilkojj@outlook.com>
2018-10-12 22:07:28 +0200

    https://pypi.org/project/HTML-as-programming-language/
    to install package:
    pip install HTML-as-programming-language
    
    to run compiler:
    htmlc
    
    to run language server:
    htmlclangsvr

---
+++ HTML_to_C_compiler/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HTML-as-programming-language',
+    version='0.1',
+    author="hilkojj",
+    author_email="hilkojj@outlook.com",
+    url="https://github.com/HTML-as-programming-language/HTML-as-programming-language",
+    description='(WIP) Write HTML-code that compiles to C',
+    long_description=open('README.txt').read(),
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'htmlclangsvr = htmlc.lang_server',
+            'htmlc = htmlc.compiler'
+        ]
+    }
+)
