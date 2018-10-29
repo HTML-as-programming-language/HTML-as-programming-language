b453a01316249528cb437163db618a0b6d0b2e32
Simon Wielenga <s.wielenga@live.nl>
2018-10-29 14:29:27 +0100

    added function for analog reading

---
+++ HTML_to_C_compiler/htmlc/data/c_code/avr/analog.c
@@ -0,0 +1,29 @@
+void analog-init()
+{
+    // AREF = AVcc
+    ADMUX = (1<<REFS0);
+
+    // ADC Enable and prescaler of 128
+    // 16000000/128 = 125000
+    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+}
+
+uint16_t analog-read(uint8_t ch)
+{
+  // select the corresponding channel 0~7
+  // ANDing with ’7′ will always keep the value
+  // of ‘ch’ between 0 and 7
+  ch &= 0b00000111;  // AND operation with 7
+  ADMUX = (ADMUX & 0xF8)|ch; // clears the bottom 3 bits before ORing
+
+  // start single convertion
+  // write ’1′ to ADSC
+  ADCSRA |= (1<<ADSC);
+
+  // wait for conversion to complete
+  // ADSC becomes ’0′ again
+  // till then, run loop continuously
+  while(ADCSRA & (1<<ADSC));
+
+  return (ADC);
+}
